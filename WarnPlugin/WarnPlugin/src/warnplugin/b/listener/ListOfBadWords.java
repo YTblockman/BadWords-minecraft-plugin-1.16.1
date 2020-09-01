76a5f056e11dea80b3de59faa84ab5d8354dcb9d
YTblockman <62651146+YTblockman@users.noreply.github.com>
2020-09-01 16:13:49 +0200

    Update ListOfBadWords.java

--- WarnPlugin/WarnPlugin/src/warnplugin/b/listener/ListOfBadWords.java
+++ WarnPlugin/WarnPlugin/src/warnplugin/b/listener/ListOfBadWords.java
@@ -6,27 +6,7 @@ public class ListOfBadWords {
 	private static ArrayList<String> ar = new ArrayList<String>();
 	
 	public static ArrayList<String> BadWordsConfig() {
-		ar.add("kokot");
-		ar.add("pica");
-		ar.add("kurva");
-		ar.add("badwordtest");
-		ar.add("debil");
-		ar.add("dement");
-		ar.add("hovno");
-		ar.add("shit");
-		ar.add("govno");
-		ar.add("poop");
-		ar.add("kkt");
-		ar.add("chobot_014");
-		ar.add("idiot");
-		ar.add("hajzel");
-		ar.add("pici");
-		ar.add("skurvenej");
-		ar.add("zajebanej");
-		ar.add("pice");
-		ar.add("pojebany");
-		ar.add("skurveny");
-		
+		ar.add("BADWORD");
 		return ar;
 	}
 }
