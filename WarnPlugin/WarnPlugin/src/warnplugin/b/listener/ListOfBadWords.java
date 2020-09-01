25d5a2a39206a3ef5b2a8563bdd893a15206b183
YTblockman <62651146+YTblockman@users.noreply.github.com>
2020-09-01 16:11:21 +0200

    Update
    This update add ban queue!

--- WarnPlugin/WarnPlugin/src/warnplugin/b/listener/ListOfBadWords.java
+++ WarnPlugin/WarnPlugin/src/warnplugin/b/listener/ListOfBadWords.java
@@ -6,7 +6,26 @@ public class ListOfBadWords {
 	private static ArrayList<String> ar = new ArrayList<String>();
 	
 	public static ArrayList<String> BadWordsConfig() {
-		ar.add("nadavka") //here goes ur list of badwords
+		ar.add("kokot");
+		ar.add("pica");
+		ar.add("kurva");
+		ar.add("badwordtest");
+		ar.add("debil");
+		ar.add("dement");
+		ar.add("hovno");
+		ar.add("shit");
+		ar.add("govno");
+		ar.add("poop");
+		ar.add("kkt");
+		ar.add("chobot_014");
+		ar.add("idiot");
+		ar.add("hajzel");
+		ar.add("pici");
+		ar.add("skurvenej");
+		ar.add("zajebanej");
+		ar.add("pice");
+		ar.add("pojebany");
+		ar.add("skurveny");
 		
 		return ar;
 	}
